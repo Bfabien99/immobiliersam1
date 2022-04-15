187dd7d358173346ac7332560d369d1cf8b4bc6d
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-04-15 13:39:31 +0000

    Project finished

--- ancien.sql
+++ ancien.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le : ven. 15 avr. 2022 à 15:37
--- Version du serveur : 10.4.22-MariaDB
--- Version de PHP : 8.0.13
+-- Host: 127.0.0.1
+-- Generation Time: Apr 15, 2022 at 10:49 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `immobiliersam`
+-- Database: `immobiliersam`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `admin`
+-- Table structure for table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -36,7 +36,7 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `admin`
+-- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id`, `nom`, `prenoms`, `email`, `password`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `admin` (`id`, `nom`, `prenoms`, `email`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `clients`
+-- Table structure for table `clients`
 --
 
 CREATE TABLE `clients` (
@@ -61,19 +61,18 @@ CREATE TABLE `clients` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `clients`
+-- Dumping data for table `clients`
 --
 
 INSERT INTO `clients` (`id`, `nom`, `prenoms`, `pseudo`, `email`, `contact1`, `contact2`, `password`, `cree_le`) VALUES
 (1, 'brou', 'kouadio stéphane-fabien', 'brou', 'fabienbrou99@gmail.com', '0153148864', '0504774183', '8082b382bea4c85367617e7271768276', '2022-04-12 14:33:43'),
 (2, 'konan', 'arsene', 'arsene', 'arseneesaie@gmail.com', '0101010101', '', 'c3b55d35cac16f624ff737c5d7785d00', '2022-04-14 14:10:34'),
-(3, 'fabien', 'id consequatur pari', 'fabiien', 'licynusyzu@mailinator.com', '11497033614', '12073234936', '8082b382bea4c85367617e7271768276', '2022-04-14 14:56:43'),
-(4, 'essoh', 'jean penuel', 'penuel', 'penuel@gmail.com', '001001010', '00101000', '6aa8b883a5db6d90874b79ea21c52a25', '2022-04-15 10:28:54');
+(3, 'fabien', 'id consequatur pari', 'fabiien', 'licynusyzu@mailinator.com', '11497033614', '12073234936', '8082b382bea4c85367617e7271768276', '2022-04-14 14:56:43');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `interest`
+-- Table structure for table `interest`
 --
 
 CREATE TABLE `interest` (
@@ -83,21 +82,18 @@ CREATE TABLE `interest` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `interest`
+-- Dumping data for table `interest`
 --
 
 INSERT INTO `interest` (`id`, `maison_id`, `client_id`) VALUES
 (1, 2, 2),
 (3, 2, 3),
-(4, 4, 3),
-(5, 2, 1),
-(6, 4, 1),
-(7, 2, 4);
+(4, 4, 3);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `maisons`
+-- Table structure for table `maisons`
 --
 
 CREATE TABLE `maisons` (
@@ -110,17 +106,17 @@ CREATE TABLE `maisons` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `maisons`
+-- Dumping data for table `maisons`
 --
 
 INSERT INTO `maisons` (`id`, `description`, `image`, `lieu`, `contact`, `date`) VALUES
-(2, 'belle maison de mon pap', '$2y$10$jdiHi51JT7hmHb2jyo5wd.2FDC44DvVSWUCJjG7ZhDaQdYhh8CfQamaison.jpg', 'abobo', '0709167244', '2022-04-14 13:09:07'),
-(4, 'maison propre', '$2y$10$q70t3w2KxsUaanQwOEN5Xe5ck5ZlhHL2KCnVO.6Qxll3Bw1d2iI5Wkelly-sikkema-tDtwC11XjuU-unsplash.jpg', 'cocody', '+1 (395) 853-9227', '2022-04-14 15:03:29');
+(2, 'belle maison de mon papa', '$2y$10$jdiHi51JT7hmHb2jyo5wd.2FDC44DvVSWUCJjG7ZhDaQdYhh8CfQamaison.jpg', 'abobo', '0709167244', '2022-04-14 13:09:07'),
+(4, 'similique non veniam', '$2y$10$q70t3w2KxsUaanQwOEN5Xe5ck5ZlhHL2KCnVO.6Qxll3Bw1d2iI5Wkelly-sikkema-tDtwC11XjuU-unsplash.jpg', 'et est omnis irure a', '+1 (395) 853-9227', '2022-04-14 15:03:29');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `reservation`
+-- Table structure for table `reservation`
 --
 
 CREATE TABLE `reservation` (
@@ -128,38 +124,36 @@ CREATE TABLE `reservation` (
   `active` tinyint(1) DEFAULT 0,
   `fait_le` datetime DEFAULT current_timestamp(),
   `maison_id` int(11) NOT NULL,
-  `client_id` int(11) NOT NULL,
-  `date_res` date NOT NULL,
-  `heure` varchar(255) NOT NULL
+  `client_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `reservation`
+-- Dumping data for table `reservation`
 --
 
-INSERT INTO `reservation` (`id`, `active`, `fait_le`, `maison_id`, `client_id`, `date_res`, `heure`) VALUES
-(11, 1, '2022-04-15 13:09:56', 4, 1, '2022-04-17', '7 heure 0 minute'),
-(12, 0, '2022-04-15 13:29:41', 4, 3, '2022-04-29', '11 heure 30 minute'),
-(13, 0, '2022-04-15 13:33:14', 2, 3, '2022-04-26', '9 heure 15 minute');
+INSERT INTO `reservation` (`id`, `active`, `fait_le`, `maison_id`, `client_id`) VALUES
+(1, 1, '2022-04-14 14:44:40', 2, 2),
+(2, 0, '2022-04-14 14:57:29', 2, 3),
+(3, 1, '2022-04-14 15:32:29', 4, 3);
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `admin`
+-- Indexes for table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `clients`
+-- Indexes for table `clients`
 --
 ALTER TABLE `clients`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `interest`
+-- Indexes for table `interest`
 --
 ALTER TABLE `interest`
   ADD PRIMARY KEY (`id`),
@@ -167,13 +161,13 @@ ALTER TABLE `interest`
   ADD KEY `maison_id` (`maison_id`);
 
 --
--- Index pour la table `maisons`
+-- Indexes for table `maisons`
 --
 ALTER TABLE `maisons`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `reservation`
+-- Indexes for table `reservation`
 --
 ALTER TABLE `reservation`
   ADD PRIMARY KEY (`id`),
@@ -181,52 +175,52 @@ ALTER TABLE `reservation`
   ADD KEY `client_id` (`client_id`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `admin`
+-- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT pour la table `clients`
+-- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `interest`
+--
+ALTER TABLE `interest`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT pour la table `interest`
---
-ALTER TABLE `interest`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT pour la table `maisons`
+-- AUTO_INCREMENT for table `maisons`
 --
 ALTER TABLE `maisons`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT pour la table `reservation`
+-- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Contraintes pour les tables déchargées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `interest`
+-- Constraints for table `interest`
 --
 ALTER TABLE `interest`
   ADD CONSTRAINT `interest_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `interest_ibfk_2` FOREIGN KEY (`maison_id`) REFERENCES `maisons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `reservation`
+-- Constraints for table `reservation`
 --
 ALTER TABLE `reservation`
   ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`maison_id`) REFERENCES `maisons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
